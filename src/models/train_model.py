304322fcc9ccc97584a2f24494875acca934dafb
f-mtz <mtz.dev.si@gmail.com>
2025-04-18 14:16:25 -0300

    Preparando modelo para receber  parâmetros com valor default e via linha de comando

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,11 +1,27 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
+import argparse
 from sklearn.metrics import mean_squared_error, r2_score
 import math
 import xgboost
 import mlflow
 
+def parse_args():
+    parser = argparse.ArgumentParser(description='House Prices ML')
+    parser.add_argument(
+        '--learning-rate',
+        type=float,
+        default=0.3,
+        help="taxa de aprendizado para atualizar o tamanho de cada passo do boosting"
+    )
+
+    parser.add_argument(
+        '--max-depth',
+        type=int,
+        default=6,
+        help="profundidade maxima das arvores"
+    )
+    return parser.parse_args()
 
 
 df = pd.read_csv('../../data/processed/casas.csv')
@@ -14,9 +30,13 @@ y = df['preco'].copy()
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 dtrain = xgboost.DMatrix(X_train, label=y_train)
 dtest = xgboost.DMatrix(X_test, label=y_test)
+
+def main():
+args = parse_args()
 xgb_params = {}
 
-xgb_params['learning_rate'] = 0.2
+xgb_params['learning_rate'] = args.learning_rate
+xgb_params['max_depth'] = args.max_depth
 xgb_params['seed'] = 42
 
 mlflow.set_experiment('house-prices-script')
@@ -32,3 +52,6 @@ with mlflow.start_run():
     mlflow.log_metric('mse', mse)
     mlflow.log_metric('rmse', rmse)
     mlflow.log_metric('r2', r2)
+
+if __name__ == '__main__':
+    main()
